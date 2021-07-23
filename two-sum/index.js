5d00824456d6b6c09ce4d633e0e3c212217a85cd
David Dean <vermicious.knid.d@gmail.com>
2021-07-23 12:09:02 -0700

    Fix function name.

--- two-sum/index.js
+++ two-sum/index.js
@@ -1,6 +1,7 @@
 // If array has unique members then put into set/object, then loop once and do a set search for difference
 // O(n)
-const twosum = function(nums, target) {
+// Is this really O(n)? We are only talking about our own code but what about hasOwnProperty?
+const twoSum = function(nums, target) {
   const numlen = nums.length;
   let numObject = {};
 	for (let i = 0; i < numlen; i++) {
@@ -8,7 +9,6 @@ const twosum = function(nums, target) {
 		numObject[thisNum] = i;
   }
   for (let i = 0; i < numlen; i++) {
-    // console.log("i=" + i, "nums[i]=" + nums[i]);
     const diff = target - nums[i];
     if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
       return [i, numObject[diff]];
