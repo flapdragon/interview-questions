13c0b7ee213a14c84daeb7bb42ae1c1d685d7ccf
David Dean <vermicious.knid.d@gmail.com>
2021-07-27 14:47:22 -0700

    Stopping point. Added recursion solution setup.

--- longest-increasing-subsequence/index.js
+++ longest-increasing-subsequence/index.js
@@ -1,3 +1,21 @@
+// Recursive method. Start at s[1] and look backwards/left to find the most sequential numbers that are smaller than s[x].
+// I know it's not the most efficient method but it's still an interesting problem to solve.
+
+function subsequenceRecursion(sequence) {
+  console.log(sequence);
+  let streak = 0;
+  const len = sequence.length;
+  for (let i = 1; i < len; i++) {
+    const sub = sequence.slice(0, i);
+    console.log(sub);
+  }
+  return sequence;
+}
+
+// Tests
+console.log(subsequenceRecursion([ 5, 2, 3, 1, 8, 101 ]));
+console.log(subsequenceRecursion([ 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 ]));
+
 // First stab, barely can wrap my head around it. This doesn't even find the longest subsequence. It just goes in order. Brain is tired.
 // Let's call this one "Going in order wrong answer gotta start somewhere"
 function increasingSub(sequence) {
