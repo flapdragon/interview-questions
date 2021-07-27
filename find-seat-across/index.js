c1085ceae1642a6f8a397b37bc26ea4c690b91d0
David Dean <vermicious.knid.d@gmail.com>
2021-07-27 14:25:00 -0700

    Add Base 12 solution. Not super fast but works cleanly. Just don't like you know try to find too many seats at once. Hmmm how many seats are there in the world? The V8 could probably handle that.

--- find-seat-across/index.js
+++ find-seat-across/index.js
@@ -4,14 +4,14 @@
 function findAcross(seat) {
   if (seat > 12) {
     const remainder = seat % 12;
-    if (remainder <= 6) {
+    if (remainder >=1 && remainder <= 6) {
       return seat + 6;
     }
     else {
       return seat - 6;
     }
   }
-  if (seat <= 6) {
+  if (seat >= 1 && seat <= 6) {
     return seat + 6;
   }
   else {
@@ -19,13 +19,35 @@ function findAcross(seat) {
   }
 }
 
+// Tests
 console.log(findAcross(4));
-console.log(findAcross(7));
-console.log(findAcross(10));
-console.log(findAcross(12));
 console.log(findAcross(13));
-console.log(findAcross(28));
-console.log(findAcross(35));
+console.log(findAcross(44));
 
-// let answer = document.getElementById("answer-goes-here");
-// console.log(answer.innerText);
+// Going hog wild and using base 12
+// Note: >= 1 and <= 6 row logic also had to work with base 12 values, which above 6 are 7, 8, 9, a, b, 0, but it still works.
+function findAcrossBase12(seat) {
+  // Find base 12 seat value
+  const basedSeat = seat.toString(12);
+  // Get the last part because now we're all the first group
+  const comparator = basedSeat.slice(-1);
+  // Row logic
+  if (comparator >= 1 && comparator <= 6) {
+    return seat + 6;
+  }
+  else {
+    return seat - 6;
+  }
+}
+
+// Tests
+console.log(findAcrossBase12(4));
+console.log(findAcrossBase12(13));
+console.log(findAcrossBase12(44));
+
+// Write answer to HTML. I haven't been including the questions so I thought I'd start.
+let answer = document.getElementById("answer-goes-here");
+answer.innerText = "f(4) = " + findAcrossBase12(4);
+
+// Notes
+// https://stackoverflow.com/questions/1337419/how-do-you-convert-numbers-between-different-bases-in-javascript/26244940 // Use int.toString(12)
