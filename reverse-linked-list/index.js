b6e5f45505c112610b2556f97cf36fa4c1ba6660
David Dean <vermicious.knid.d@gmail.com>
2021-09-01 16:33:59 -0700

    Set up problem, README, HTML. Hard code linked list in JavaScript.

---
+++ reverse-linked-list/index.js
@@ -0,0 +1,15 @@
+// Problem
+
+// Rules
+
+// Use cases / Theorems
+
+// Solution 1: Brute Force.
+console.log({ head: { data: 1, next: { data: 2, next: { data: 3, next: { data: 4, next: null } } } } }); // 4, 3, 2, 1
+// Tests 1: Brute Force
+
+// What I learned
+
+// Notes
+// https://www.interviewbit.com/problems/reverse-linked-list/
+// https://en.wikipedia.org/wiki/Linked_list
