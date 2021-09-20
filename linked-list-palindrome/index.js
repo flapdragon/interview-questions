1fa8cb737e79ed94e76d2d78aa2c5f76fbf4d7dc
David Dean <vermicious.knid.d@gmail.com>
2021-09-20 13:52:53 -0700

    Add linked list palindrome problem. Add README, HTML and placeholder JS.

---
+++ linked-list-palindrome/index.js
@@ -0,0 +1,25 @@
+// Problem.
+// Given the head of a singly linked list, return true if it is a palindrome, otherwise false.
+// Rules
+// JavaScript doesn't have this datatype but we can imitate it with a simple object with each next list member being nested in the previous object.
+// The first and outermost object is the head.
+// Each item should have 2 attributes, data and next.
+// There is no permutation, so we determine palindromocity with the list as is..
+
+// Use cases / Theorems
+
+// Solution 1:
+
+// Tests 1: Iterative
+
+
+
+
+
+// Performance
+
+
+// What I learned
+
+
+// Notes
